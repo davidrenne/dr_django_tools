b4cc1132b7bf6181c26a39758ab97b098528fc0a
David Renne <david_renne@yahoo.com>
2015-02-19 17:07:26 -0500

    package name

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setup(
         'dr_django_tools',
         'dr_django_tools.shared',
         'dr_django_tools.shared.commondata',
-        'dr_django_tools.django',
+        'dr_django_tools.shared.django',
     ],
     package_dir={'dr-django-tools':
                  'dr_django_tools'},
